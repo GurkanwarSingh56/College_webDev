7e12c086b5bd3d459389f8b613d460e04a915705
Gurkanwar Singh <157824528+GurkanwarSingh56@users.noreply.github.com>
2025-07-07 13:16:28 +0530

    day 6

---
+++ day 6/script.js
@@ -0,0 +1,27 @@
+
+//         document.getElementById("heading").innerText=("hi from gavi");
+//        const a= document.getElementById("heading")
+//        a.style.border="2px solid red";
+//        a.style.textAlign="center";
+// const myFunction = () => {
+//         let a=10;
+//         var b=40;
+//         console.log(a+b);
+// }
+// console.log(b);
+// console.log(url);
+// myFun()
+// let a=10;
+// console.log(a)
+// console.log(b)
+// var b=50;
+// const c=30;
+// console.log(c);
+function myname(){
+        comsole.log("gavi")
+}
+function myFun(){
+        myname()
+console.log("hi from gavi")
+}
+myFunction()
